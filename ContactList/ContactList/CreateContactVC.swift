557de6b76c9c26f3298ade855f5717dcd3c4ad42
Maxim Vitovitsky <maxim@twincorp.ru>
2019-11-13 23:35:15 +0500

    Code review.

--- ContactList/ContactList/CreateContactVC.swift
+++ ContactList/ContactList/CreateContactVC.swift
@@ -15,21 +15,8 @@ class CreateContactVC: UIViewController {
     @IBOutlet weak var phoneField: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
     
-    @IBAction func saveContact(_ sender: Any) {
-        if (nameField.text != "" && phoneField.text != "") {
-            if (phoneField.text?.count != 11) {
-                errorLabel.text = "Phone nuber should contain 11 numbers"
-            }
-            else {
-                Contact.shared.append(Contact(name: nameField.text, surname: surnameField.text, number: phoneField.text))
-                self.navigationController?.popViewController(animated: true)
-            }
-        }
-        else {
-            errorLabel.text = "You can not make new contact without name and phone number\nPlease fill these fields"
-        }
-    }
     
+//    Методы жизненного цикла контроллера переносим выше
     override func viewDidDisappear(_ animated: Bool) {
         errorLabel.text = ""
     }
@@ -37,6 +24,30 @@ class CreateContactVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.phoneField.delegate = self
+    }
+    
+//    А дальше уже идут пользовательские события
+    @IBAction func saveContact(_ sender: Any) {
+        if (nameField.text != "" && phoneField.text != "") {
+            if (phoneField.text?.count != 11) {
+                errorLabel.text = "Phone nuber should contain 11 numbers"
+            } else { // Else обычно остаётся на той же строке, что и скобка
+                
+                // Для удобства чтения лучше сделать вот так:
+                
+                let newContact = Contact(
+                    name: nameField.text,
+                    surname: surnameField.text,
+                    number: phoneField.text
+                )
+                Contact.shared.append(newContact)
+                
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+        else {
+            errorLabel.text = "You can not make new contact without name and phone number\nPlease fill these fields"
+        }
     }
 
 }
